4ed693fe4c244d2bcba4ca08aee6c66bd2b5b719
Raz <amsalemraz10@gmail.com>
2024-01-25 17:49:43 +0200

    Improving the user experience on the search page in case nothing is available yet

--- screens/SearchPage.tsx
+++ screens/SearchPage.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import { View } from "react-native"
+import React, { useState } from "react"
+import { Image, Text, View, StyleSheet } from "react-native"
 
 import { VideoItem } from "../types/VideoItem"
 import SearchBar from "../components/SearchBar"
@@ -15,7 +15,28 @@ export default function SearchPage() {
     return (
         <View>
             <SearchBar onSearchResult={onSearchResult} />
-            <VideoList videos={userSearch} />
+            {userSearch.length > 0 ? (
+                <VideoList videos={userSearch} />
+            ) : (
+                <View style={styles.imageContainer}>
+                    <Image
+                        source={require("../assets/search.png")}
+                        style={styles.fullHeightImage}
+                    />
+                </View>
+            )}
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    imageContainer: {
+        alignItems: "center",
+        marginTop: 50,
+        height: "100%",
+    },
+    fullHeightImage: {
+        width: 256,
+        height: 256,
+    },
+})
