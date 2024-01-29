887778c09571947a2df03ed43dacdca5ac30e9a0
Raz <amsalemraz10@gmail.com>
2024-01-29 14:38:38 +0200

    Fix icon and title not in the same line

--- screens/UserScreen.tsx
+++ screens/UserScreen.tsx
@@ -148,7 +148,7 @@ const styles = StyleSheet.create({
     },
     header: {
         flexDirection: 'row',
-        alignItems: 'center',
+        alignItems: 'baseline',
         gap: 6,
         paddingHorizontal: SIZES.medium,
     },
